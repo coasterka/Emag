e91a023e1d3c65faf76546089824843404849aa6
coasterka <tsvetanovah@gmail.com>
2016-02-02 13:38:13 +0200

    Added IUser interface to project

---
+++ proekt/classes/IUser.java
@@ -0,0 +1,7 @@
+
+public interface  IUser {
+	void displayCart();
+	boolean validateLogin(String username, String password) throws EmagInvalidPassException;
+	public void updateProfile(String name, String username, String address);
+	public void changePassword(String oldPass, String passwordNew1, String passwordNew2);
+}
